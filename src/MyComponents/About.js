96c29524089f7ab05aac197cad08c56c642c5e75
Vishal_Singh <vishal30wish@gmail.com>
2022-09-04 09:03:10 +0530

    first_commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>This site will help to remind you of the work you are going to do.
+                You can easily delete the reminder any time you want and create a new one as well. </p>
+        </div>
+    )
+}
